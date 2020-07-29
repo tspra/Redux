2790c28217eedd0ccd2be5542d457a0fa53cca11
Prajeesh Thaniyath <Prajeesh.Thaniyath@neudesic.com>
2020-07-29 16:20:20 +0530

    Decrement action added

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import {useSelector,useDispatch} from 'react-redux'
-import Increment from './actions'
+import Increment from './actions/increment'
+import Decrement from './actions/decrement'
 
 function App() 
 {
@@ -14,6 +15,7 @@ function App()
       </header>
   <h5>Counter {counter}</h5>
       <button onClick={()=> dispatch(Increment())}>+</button>
+      <button onClick={()=> dispatch(Decrement())}>-</button>
     </div>
   );
 }
---
+++ react-redux-app/src/actions/decrement.js
@@ -0,0 +1,16 @@
+const Decrement = ()=>
+{
+    return{
+        type : 'Decrement'
+    };
+};
+
+export default Decrement;
+
+// const Increment= ()=>
+// {
+//     return {
+//         type : 'Increment'
+//     };
+// };
+// export default Increment;
